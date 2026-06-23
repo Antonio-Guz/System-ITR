d688f7f84e7ff786ca2b4b9e7bc978f075fc02c8
Eduardo <20220498@ricaldone.edu.sv>
2026-06-23 09:24:31 -0600

    Primer commit

---
+++ src/main/java/SystemITR/EduardoGuzman/EduardoGuzmanApplication.java
@@ -0,0 +1,13 @@
+package SystemITR.EduardoGuzman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduardoGuzmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduardoGuzmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SystemITR/EduardoGuzman/EduardoGuzmanApplicationTests.java
@@ -0,0 +1,13 @@
+package SystemITR.EduardoGuzman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduardoGuzmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
